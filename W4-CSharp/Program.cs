f3fc0fa507ce64758935ac380496b8728301f96b
Adam Sepulveda <adam.sepulveda@revature.net>
2021-09-10 11:43:55 -0500

    Adding Week 4 Challenge

---
+++ W4-CSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace W4_CSharp
+{
+    class Program
+    {
+        static string GiveMeSomething(string str)
+        {
+            string result = "something " + str;
+            System.Console.WriteLine(result);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            GiveMeSomething("is better than nothing");
+
+            GiveMeSomething("Bob Jane");
+
+            GiveMeSomething("something");
+        }
+    }
+}
